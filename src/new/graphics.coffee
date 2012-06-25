75ecfef43d5d8f1a98a177dcd26d354c53f314df
Maros Hluska <mhluska@gmail.com>
2012-06-24 22:43:29 -0400

    Implemented DOM graphics.

--- src/new/graphics.coffee
+++ src/new/graphics.coffee
@@ -1,5 +1,39 @@
 window.Game ?= {}
 class Game.Graphics
 
-    constructor: (@grid) ->
+    constructor: (@grid, @graphicsType = 'dom') ->
+
+       @buildDOMElems() if @graphicsType is 'dom'
+
     update: ->
+
+    buildDOMElems: ->
+
+        @dom = {}
+        @dom.grid = $('<div id="grid"></div>')
+        @dom.grid.css
+            width: @grid.squareWidth * @grid.squaresX
+            height: @grid.squareHeight * @grid.squaresY
+
+        @dom.squares = []
+        for column, squareX in @grid.world
+            for square, squareY in column
+
+                continue if $.isEmptyObject square
+
+                type = 'snake' if square.snake
+                type = 'food' if square.food
+
+                elem = x: squareX, y: squareY, type: type
+                elem.node = $("<div class='#{type}'></div>")
+                elem.node.css x: elem.x, y: elem.y
+
+                @dom.squares.push elem
+                @dom.grid.append elem.node
+
+        $('body').append @dom.grid
+                
+
+
+                
+
--- src/new/graphics.js
+++ src/new/graphics.js
@@ -4,11 +4,47 @@
 
   Game.Graphics = (function() {
 
-    function Graphics(grid) {
+    function Graphics(grid, graphicsType) {
       this.grid = grid;
+      this.graphicsType = graphicsType != null ? graphicsType : 'dom';
+      if (this.graphicsType === 'dom') this.buildDOMElems();
     }
 
     Graphics.prototype.update = function() {};
+
+    Graphics.prototype.buildDOMElems = function() {
+      var column, elem, square, squareX, squareY, type, _len, _len2, _ref;
+      this.dom = {};
+      this.dom.grid = $('<div id="grid"></div>');
+      this.dom.grid.css({
+        width: this.grid.squareWidth * this.grid.squaresX,
+        height: this.grid.squareHeight * this.grid.squaresY
+      });
+      this.dom.squares = [];
+      _ref = this.grid.world;
+      for (squareX = 0, _len = _ref.length; squareX < _len; squareX++) {
+        column = _ref[squareX];
+        for (squareY = 0, _len2 = column.length; squareY < _len2; squareY++) {
+          square = column[squareY];
+          if ($.isEmptyObject(square)) continue;
+          if (square.snake) type = 'snake';
+          if (square.food) type = 'food';
+          elem = {
+            x: squareX,
+            y: squareY,
+            type: type
+          };
+          elem.node = $("<div class='" + type + "'></div>");
+          elem.node.css({
+            x: elem.x,
+            y: elem.y
+          });
+          this.dom.squares.push(elem);
+          this.dom.grid.append(elem.node);
+        }
+      }
+      return $('body').append(this.dom.grid);
+    };
 
     return Graphics;
 
