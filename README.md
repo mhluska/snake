8621fd24d5debce0bf2db261bb8638b71ca9756b
Maros Hluska <mhluska@gmail.com>
2015-12-07 13:10:28 -0500

    Add base Webpack files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # snake
-3D self-playing snake game.
+3D self-playing snake game
+
+### Develop
+```
+npm install
+npm run build
+npm start
+```
---
+++ snake-init.js
@@ -0,0 +1,5 @@
+(function () {
+  // TODO(maros): Avoid global `Snake` variable.
+  // var game = new Snake(document.querySelector('.snake-game'))
+  // game.run();
+})();
